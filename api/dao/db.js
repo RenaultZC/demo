cd129fb0fdb9eb2b30df34b43b01843b66f7fbce
RenaultZC <1073294992@qq.com>
2019-01-21 11:39:36 +0800

    new

--- api/dao/db.js
+++ api/dao/db.js
@@ -1,22 +1,22 @@
-const mysql = require('mysql2');
+﻿const mysql = require('mysql2');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '19980527',
-    database: 'test',
-    port: '3306'
+    host: '*****',
+    user: '*****',
+    password: '*****',
+    database: '*****',
+    port: '*****'
 });
 
 module.exports = async function main() {
     const mysql = require('mysql2/promise');
     // 创建连接
     const conn = await mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: '19980527',
-        database: 'test',
-        port: '3306'}
-    );
+        host: '*****',
+        user: '*****',
+        password: '*****',
+        database: '*****',
+        port: '*****'
+});
     //查询
     const [rows, fields] = await conn.execute('SELECT * FROM `user`')
     return rows;
